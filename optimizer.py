80e3da5d1a69e729587d3b310cf19d4ca630589b
DenysKaliuzhnyi <denyskaliuzhnyi@gmail.com>
2022-01-17 19:33:21 +0200

    modify base interface to replicate scikit-learn

--- optimizer.py
+++ optimizer.py
@@ -1,23 +1,21 @@
 import numpy as np
 from abc import ABC, abstractmethod
+from scipy.spatial import distance_matrix
 
 
 class Optimizer(ABC):
 
-    def __init__(self, k: int):
-        self.k = k
-
-    def fit(self, X: np.ndarray):
-        self.X = X
+    def __init__(self, X: np.ndarray):
+        self.X = np.copy(X)
 
     @abstractmethod
-    def get_k_nearest(self, X: np.ndarray) -> np.ndarray:
+    def query(self, X: np.ndarray, k: int) -> np.ndarray:
         pass
 
 
 class LinearOptimizer(Optimizer):
 
-    def get_k_nearest(self, X: np.ndarray) -> np.ndarray:
-        distances = np.linalg.norm(self.X - X, axis=1)
-        indices = np.argsort(distances)[:self.k]
+    def query(self, X: np.ndarray, k=1) -> np.ndarray:
+        distances = distance_matrix(X, self.X)
+        indices = np.argsort(distances, axis=1)[:,:k]
         return indices
